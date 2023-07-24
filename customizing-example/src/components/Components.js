80dd6fe44803d183c3e6d7504d7e10964cc6dafa
cd155 <cd155@163.com>
2023-07-24 18:25:13 -0400

    add logo

--- customizing-example/src/components/Components.js
+++ customizing-example/src/components/Components.js
@@ -5,5 +5,6 @@ import Intro2 from './Intro2';
 import Intro3 from './Intro3';
 import Nav from './Nav';
 import Promo from './Promo';
+import Logo from './Logo';
 
-export{Footer, Heading, Intro1, Intro2, Intro3, Nav, Promo}
+export { Footer, Heading, Intro1, Intro2, Intro3, Nav, Promo, Logo }
