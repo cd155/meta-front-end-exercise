3485e34cbb0aa17f0d25abf17fe0ed0836c706b7
cd155 <cd155@163.com>
2023-07-24 16:22:34 -0400

    group components in one file

---
+++ customizing-example/src/components/Components.js
@@ -0,0 +1,9 @@
+import Footer from './Footer';
+import Heading from './Heading';
+import Intro1 from './Intro1';
+import Intro2 from './Intro2';
+import Intro3 from './Intro3';
+import Nav from './Nav';
+import Promo from './Promo';
+
+export{Footer, Heading, Intro1, Intro2, Intro3, Nav, Promo}
