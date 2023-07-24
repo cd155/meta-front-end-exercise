fbb455742d51198a6f4986c9e53ef22d03aff6fa
cd155 <cd155@163.com>
2023-07-24 16:23:09 -0400

    add Promo

--- customizing-example/src/App.js
+++ customizing-example/src/App.js
@@ -1,9 +1,11 @@
-import Heading from './components/Heading';
+import {Heading, Promo} from './components/Components';
+
 import './App.css';
 
 function App(props) {
   return (
     <div>
+      <Promo />
       <h1>{props.title}</h1>
       <Heading name="Pie" />
     </div>
